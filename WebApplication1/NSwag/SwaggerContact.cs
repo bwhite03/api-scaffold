0c5430a539e97bf9e588f17c28849ea8f86a14be
C5m7b4 <mbedingfield@dcrpos.com>
2022-02-23 09:26:06 -0600

    Inital commit

---
+++ WebApplication1/NSwag/SwaggerContact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.NSwag
+{
+    public class SwaggerContact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ WebApplication1/NSwag/SwaggerLicense.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.NSwag
+{
+    public class SwaggerLicense
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
